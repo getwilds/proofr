11914b3d44d98d0283e23f8671b107d064bb8d9e
Scott Chamberlain <sachamber@fredhutch.org>
2024-01-25 11:38:22 -0800

    fix #6 change pi_name param to slurm_account in proof_start

--- R/start.R
+++ R/start.R
@@ -1,21 +1,24 @@
 #' Start PROOF Cromwell server
 #'
 #' @export
-#' @param pi_name (character) PI name in the form last_f; only needed if user
-#' is in more than one SLURM account
-#' @references <https://github.com/FredHutch/proof-api#post-cromwell-server>
+#' @param slurm_account (character) PI name in the form `last_f`, where last
+#' is the last name of the PI and f is the first name, e.g., "Jane Doe"
+#' would be `doe_j`; only needed if user is in more than one SLURM account.
+#' optional
+#' @references <https://github.com/FredHutch/proof-api/#post-cromwell-server>
 #' @details Does not return PROOF/Cromwell server URL, for that you have to
 #' periodically call [proof_status()], or wait for the email from the
-#' PROOF API
+#' PROOF API. See the link to proof-api for more details about the
+#' slurm account.
 #' @inheritSection proof_status Timeout
 #' @return A list with fields:
-#' - `job_id` (character)
-#' - `info` (character)
-proof_start <- function(pi_name = NULL) {
+#' - `job_id` (character) - the job ID
+#' - `info` (character) - message
+proof_start <- function(slurm_account = NULL) {
   response <- POST(
     make_url("cromwell-server"),
     proof_header(),
-    body = list(pi_name = pi_name),
+    body = list(slurm_account = slurm_account),
     encode = "json",
     timeout(proofr_env$timeout_sec)
   )
