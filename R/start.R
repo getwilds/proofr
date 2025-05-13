007430d11cf24b23a4ba4a574c17f0693d36f083
dtenenba <dtenenba@fhcrc.org>
2025-05-13 12:58:03 -0700

    break up another long line

--- R/start.R
+++ R/start.R
@@ -28,7 +28,8 @@
 proof_start <- function(slurm_account = NULL,
                         token = NULL, regulated_data = FALSE) {
   request(make_url("cromwell-server")) |>
-    req_body_json(list(slurm_account = slurm_account, regulated_data = regulated_data)) |>
+    req_body_json(list(slurm_account = slurm_account,
+                 regulated_data = regulated_data)) |>
     proof_header(token) |>
     req_timeout(proofr_env$timeout_sec) |>
     req_error(body = error_body) |>
